b69eb8b7572c5bb8ec5d1afcb904d89f8857cd3f
Manvendra Singh Yadav <34604538+f608253@users.noreply.github.com>
2018-12-10 22:35:48 +0530

    Update ping_hostnames_and_report.sh

--- ping_hostnames_and_report.sh
+++ ping_hostnames_and_report.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+ping_targets="172.31.16.15 172.31.25.238 172.31.16.12"
+failed_hosts=""
+
+for i in $ping_targets
+do
+   ping -c 1 $i > /dev/null
+   if [ $? -ne 0 ]; then
+      if [ "$failed_hosts" == "" ]; then
+         failed_hosts="$i"
+      else
+         failed_hosts="$failed_hosts, $i"
+      fi
+   fi
+done
+
+if [ "$failed_hosts" != "" ]; then
+   echo $failed_hosts are not reachable
+fi
 
